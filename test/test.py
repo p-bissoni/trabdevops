d9d91236b65066e07cfc7bb38c8c21f8f3a8f147
p-bissoni <patrickbissoni20073@gmail.com>
2024-09-27 18:32:02 -0300

    Incluindo testes unitários

--- test/test.py
+++ test/test.py
@@ -13,7 +13,7 @@ def test_funcaoteste():
 
 def test_create_estudante():
     estudante_teste: Estudante(name="Fulano", curso="Curso 1", ativo=False)
-    assert estudante_teste == create_estudante() 
+    assert estudante_teste == create_estudante(estudante_teste) 
 
 def test_update_estudante_negativo():
     assert update_estudante(-5)
